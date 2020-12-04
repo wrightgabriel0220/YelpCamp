6bf6dd3c9233a6a8da9d1388fc1a485761c57abb
wrightgabriel0220 <gabrielthomas.wright@gmail.com>
2020-12-04 13:09:13 -0800

    Styles and expanded camp info
    Finished the bootstrap styles across all pages and expanded new properties of CampgroundSchema and/or implemented them more fully in CRUD.

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
-const { places, descriptors } = require('./seedhelpers');
+const { places, descriptors } = require("./seedhelpers");
 const cities = require("./cities");
+const axios = require("axios");
+const express = require("express");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -21,14 +23,19 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Magna eiusmod enim laborum mollit ea eiusmod laboris labore occaecat irure non magna cillum laboris. Eiusmod est officia sit consectetur eu veniam nulla. Aliqua laborum adipisicing magna aliqua dolore nisi Lorem ullamco excepteur.",
+      price,
     });
     await camp.save();
   }
 };
 
 seedDB().then(() => {
-    mongoose.connection.close();
+  mongoose.connection.close();
 });
