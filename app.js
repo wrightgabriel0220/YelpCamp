51a2826888ff640c174b0db4b4bb038478a92039
wrightgabriel0220 <gabrielthomas.wright@gmail.com>
2020-12-05 12:50:44 -0800

    modified:   app.js 	modified:   views/campgrounds/edit.ejs 	modified:   views/campgrounds/index.ejs 	modified:   views/campgrounds/new.ejs

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.get('/campgrounds/:id/edit', async(req, res) => {
 
 app.put("/campgrounds/:id", async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}, { useFindAndModify: false });
     res.redirect(`/campgrounds/${campground.id}`);
 })
 
